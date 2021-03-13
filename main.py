35e35caf57600b24429a56ddddf7fd62bc1d8dbc
Legorel <Legorel412@gmail.com>
2021-03-13 22:37:55 +0100

    Added base project list screen

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import os
 from os import path
+from os.path import abspath
 
 import kivy
+
+kivy.require("1.11.1")
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.logger import Logger
+from kivy.lang import Builder
+
+from project import Project
 
 
 def info(text):
@@ -12,32 +18,46 @@ def info(text):
 
 
 class KivyCreator(App):
+  def __init__(self):
+    super().__init__()
+    self.default_dir = None
+    self.project_dir = None
+    self.projects = None
+    self.current_project = None
+
   def build(self):
-    self.kv_directory = path.join(self.user_data_dir, "kv")
+    self.default_dir = abspath(os.getcwd())
+    self.kv_directory = path.join(self.default_dir, "kv")
+    self.kv_file = path.join(self.kv_directory, "main.kv")
     self.title = "KivyCreator"
     self.set_project_dir()
     self.read_project_dir()
-    return Button(text=str(self.projects))
+    return Builder.load_file(self.kv_file)
 
   def set_project_dir(self):
     self.project_dir = path.join(self.user_data_dir, "projects")
-    info(self.user_data_dir)
-    info(self.project_dir)
     if not path.isdir(self.project_dir):
       os.mkdir("projects")
 
   def read_project_dir(self):
-    self.projects = folders = [f for f in os.listdir(self.project_dir) if path.isdir(path.join(self.project_dir, f))]
+    self.projects = [f for f in os.listdir(self.project_dir) if path.isdir(path.join(self.project_dir, f))]
 
   def open_project(self, name):
     if name in self.projects:
       #switch screens
-      #open files
+      project_dir = path.join(self.project_dir, name)
+      self.current_project = Project(project_dir).open()
 
   def save_project(self):
-    pass
+    #TODO: Save popup
+    #TODO: read new content
+    new_tab_content = {}
+    self.current_project.save(new_tab_content)
 
   def close_project(self):
+    self.save_project()
+    self.current_project = None
+    os.chdir(self.default_dir)
     
 
 if __name__ == "__main__":
--- project.py
+++ project.py
@@ -1,32 +1,43 @@
-class Project():
-  def __init__(self, folder_name):
-    self.folder_name = folder_name
+from os import chdir
+
+class Project:
+  def __init__(self, project_dir):
+    self.project_dir = project_dir
     self.tabs = {}
     self.get_last_tabs()
 
-  def get_last_tabs():
+  def get_last_tabs(self):
     with open("tabs.kct", "r") as f:
       lines = [x.strip() for x in f.readLines()]
       for t in lines:
         self.tabs[t] = Tab(t)
 
-  def update_tab(name, new_content):
+  def update_tab(self, name, new_content):
     if name in self.tabs:
       self.tabs[name].update_content(new_content)
 
+  def open(self):
+    chdir(self.project_dir)
 
-class Tab():
+  def save(self, tab_content):
+    for k in tab_content.keys():
+      if k in self.tabs:
+        self.tabs[k].update_content(tab_content[k])
+
+
+class Tab:
   def __init__(self, name):
     self.name = name
+    self.content = None
     self.get_content()
 
-  def get_content():
+  def get_content(self):
     with open(self.name, "r") as c:
       self.content = c
 
-  def update_content(new_content):
+  def update_content(self, new_content):
     self.content = new_content
 
-  def save_content():
+  def save_content(self):
     with open(self.name, "w") as f:
       f.write(self.content)
